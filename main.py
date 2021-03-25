05116e2888896386b069452cd1c7fe9cf2ff9d72
r08922129 <r08922129@csie.ntu.edu.tw>
2021-03-26 06:21:19 +0800

    build dataset

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from src.data import SyntaxMappingDataset
+from src.data.dataset import SyntaxMappingDataset
+from src.data.syntax import Production
+from src.utils.preprocess import collectSymbols
+import json
+import re
+from tqdm import tqdm
+import csv
 
+def loadSymbols(pathToSymbols):
+
+    with open(pathToSymbols) as f:
+        out = [symbol.strip() for symbol in f.readlines()]
+
+    return out
 
 if __name__ == "__main__":
 
-    dataset = 
+    
+    symbols = loadSymbols("save/symbols")
+    # build dataset
+    trainDataset = SyntaxMappingDataset(
+        pathToSource="syntax_data/QQPPos/train/train-src",
+        pathToReference="syntax_data/QQPPos/train/train-ref",
+        symbols=symbols,
+    )
+    
--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,13 +1,14 @@
 import os
 import argparse
 import json
-import re
 import sys
+from nltk import Tree
+from functools import reduce
+import re
 
 def preprocessCoreNLPTree(mapping):
 
-    '''Remove the postfix of symbols of the output dict of CoreNLP.
-    If a non-terminal symbol's right-hand-side is a terminal word, it won't be
+    '''If a non-terminal symbol's right-hand-side is a terminal word, it won't be
     a key in the result.
     
     Args:
@@ -20,7 +21,7 @@ def preprocessCoreNLPTree(mapping):
 
     Return:
         eg. {
-            "ROOT" : ["NP", "VP"],
+            "ROOT" : ["NP-1", "VP-1"],
         }
     '''
     def childIsTerminal(root, mapping):
@@ -29,15 +30,64 @@ def preprocessCoreNLPTree(mapping):
             if child in mapping:
                 return False
         return True
-  
-    def removePostfix(node):
-        return re.sub(r"-.*", '', node)
 
     out = {}
     for symbol in mapping:
-        reduceSymbol = removePostfix(symbol)
         if not childIsTerminal(symbol, mapping):
-            out[reduceSymbol] = []
+            out[symbol] = []
             for child in mapping[symbol]:
-                out[reduceSymbol].append(removePostfix(child))
+                out[symbol].append(child)
+    return json.dumps(out)
+
+def collectSymbols(sample, cnf=False):
+
+    def dfs(root, sample):
+        if root not in sample:
+            return Tree(root, [])
+        else:
+            return Tree(root, [dfs(child, sample) for child in sample[root]])
+
+    tree = dfs("ROOT", sample)
+    if cnf:
+        Tree.chomsky_normal_form(tree)
+
+    out = set()
+    for production in tree.productions():
+        lhs, rhs = production.lhs(), production.rhs()
+        if rhs:
+            rhs = ' '.join([re.sub(r"-\d+", '', nonterminal.symbol()) for nonterminal in rhs])
+        out.add(rhs)
+
     return out
+
+def collectSymbolsFromDataset(files, outputFile, cnf=False):
+
+    out = set()
+    for file in files:
+        with open(file) as f:
+            for line in f.readlines():
+                sample = json.loads(line)
+                out.update(collectSymbols(sample, cnf))
+    with open(outputFile, "w") as f:
+        f.write("ROOT\n")
+        for i, symbol in enumerate(list(out), 1):
+            f.write("{}\n".format(symbol))
+    return list(out)
+
+
+if __name__ == "__main__":
+
+    # remove terminal words
+    # for line in sys.stdin.readlines():
+    #     print(preprocessCoreNLPTree(json.loads(line)))
+
+    # generate set of symbols
+    files = [
+        "syntax_data/QQPPos/train/train-src",
+        "syntax_data/QQPPos/train/train-ref",
+        "syntax_data/QQPPos/val/val-src",
+        "syntax_data/QQPPos/val/val-ref",
+        "syntax_data/QQPPos/test/test-src",
+        "syntax_data/QQPPos/test/test-ref",
+    ]
+    collectSymbolsFromDataset(files, "symbols")
